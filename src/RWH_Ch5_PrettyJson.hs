4bbc0e8e4f3c0d729902181cd63c45e43821bacd
Konstantin Silin <konstantin.silin@gmail.com>
2018-09-07 17:19:38 +0200

    WIP: rwh ch5 prettyjson

--- src/RWH_Ch5_PrettyJson.hs
+++ src/RWH_Ch5_PrettyJson.hs
@@ -3,6 +3,8 @@ module RWH_Ch5_PrettyJson where
 import Data.List (intercalate)
 import RWH_Ch5_Json
 import Numeric
+import Data.Bits
+import Data.Char
 
 
 data Doc = Empty
@@ -61,9 +63,33 @@ simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
                 where ch a b = (a, ['\\', b])
 
 hexEscape :: Char -> Doc
-hexEscape = undefined
+hexEscape c | d < 0x10000 = smallHex d
+            | otherwise   = astral (d - 0x10000)
+  where d = ord c
+
+smallHex :: Int -> Doc
+smallHex x  = text "\\u"
+           <.> text (replicate (4 - length h) '0')
+           <.> text h
+    where h = showHex x ""
+
+astral :: Int -> Doc
+astral n = smallHex (a + 0xd800) <.> smallHex (b + 0xdc00)
+    where a = (n `shiftR` 10) .&. 0x3ff
+          b = n .&. 0x3ff
+
+series :: Char -> Char -> (a -> Doc) -> [a] -> Doc
+series open close item = enclose open close
+                       . fsep . punctuate (char ',') . map item
+
+fsep :: [Doc] -> Doc
+fsep xs = undefined
 
 
+punctuate :: Doc -> [Doc] -> [Doc]
+punctuate p []     = []
+punctuate p [d]    = [d]
+punctuate p (d:ds) = (d <.> p) : punctuate p ds
 
 --renderJValue :: JValue -> Doc
 --
