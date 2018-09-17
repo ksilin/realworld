422b29208e3e7f5d7f5297e16f4a2dd663bf0029
Konstantin Silin <konstantin.silin@gmail.com>
2018-09-17 18:46:18 +0200

    WIP - ch5 prettyJson

--- src/RWH_Ch5_PrettyJson.hs
+++ src/RWH_Ch5_PrettyJson.hs
@@ -1,55 +1,26 @@
-module RWH_Ch5_PrettyJson where
+module RWH_Ch5_PrettyJson
+(
+  renderJValue
+)where
 
 import Data.List (intercalate)
+import Numeric (showHex)
+import Data.Bits (shiftR, (.&.))
+import Data.Char (ord)
+
 import RWH_Ch5_Json
-import Numeric
-import Data.Bits
-import Data.Char
+import RWH_Ch5_Prettify (Doc, (<.>), char, double, fsep, hcat, punctuate, text,
+                 compact, pretty)
 
+-- TODO - remove?
 
-data Doc = Empty
-         | Char Char
-         | Text String
-         | Line
-         | Concat Doc Doc
-         | Union Doc Doc
-         deriving (Show, Eq)
-
-empty :: Doc
-empty = Empty
-
--- string is written in a point-free style, by combining fns
--- 'point' is roughly synonymous with 'value' -> a point-free expression makes no mention of values it operates on
 string :: String -> Doc
 string = enclose '"' '"' . hcat . map oneChar
 
 enclose :: Char -> Char -> Doc -> Doc
 enclose left right x = char left <.> x <.> char right
 
-(<.>) :: Doc -> Doc -> Doc
-Empty <.> y = y
-x <.> Empty = x
-x <.> y = x `Concat` y
-
-
-char :: Char -> Doc
-char = Char
-
-text :: String -> Doc
-text "" = Empty
-text str = Text str
-
-double :: Double -> Doc
-double d = text (show d)
-
-hcat :: [Doc] -> Doc
-hcat xs = undefined
-
-fold :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
-fold f = foldr f empty
-
-line :: Doc
-line = Line
+--
 
 oneChar :: Char -> Doc
 oneChar c = case lookup c simpleEscapes of
@@ -82,30 +53,20 @@ series :: Char -> Char -> (a -> Doc) -> [a] -> Doc
 series open close item = enclose open close
                        . fsep . punctuate (char ',') . map item
 
-fsep :: [Doc] -> Doc
-fsep xs = undefined
 
+renderJValue :: JValue -> Doc
 
-punctuate :: Doc -> [Doc] -> [Doc]
-punctuate p []     = []
-punctuate p [d]    = [d]
-punctuate p (d:ds) = (d <.> p) : punctuate p ds
+renderJValue (JBool True)  = text "true"
+renderJValue (JBool False) = text "false"
+renderJValue JNull         = text "null"
+renderJValue (JNumber num) = double num
+renderJValue (JString str) = string str
 
---renderJValue :: JValue -> Doc
---
---renderJValue (JString s) = string s
---renderJValue (JNumber n) = double n
---renderJValue (JBool True) = text "true"
---renderJValue (JBool False) = text "false"
---renderJValue JNull = text "null"
---
---renderJValue (JObject o) = "{" ++ pairs o ++ "}"
---  where pairs [] = ""
---        pairs ps = intercalate ", " (map renderPair ps)
---        renderPair (k, v) = show k ++ ": " ++ renderJValue v
---
---renderJValue (JArray a) = "[" ++ values a ++ "]"
---  where values [] = ""
---        values vs = intercalate ", " (map renderJValue vs)
+renderJValue (JArray ary) = series '[' ']' renderJValue ary
+
+renderJValue (JObject obj) = series '{' '}' field obj
+    where field (name,val) = string name
+                          <.> text ": "
+                          <.> renderJValue val
 
 
